ee26b28eeaebb14e3276c145b9f6f1f892fa8fa7
yasaswini <gyasu1110@gmail.com>
2025-01-28 15:05:19 +0530

    coding in react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home1 from './components/Home1';
-import Tenants from './components/Tenants';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home1 from "./components/Home1";
+import Tenants from "./components/Tenants";
 
 function App() {
   return (
     <Router>
-      {/* Navbar remains intact on all pages */}
-      <Home1 />
-
-      {/* This will render different content based on the route */}
+      {" "}
+      <Home1 />{" "}
       <Routes>
-        <Route path="/" element={<h1>Wsjd
-          
-        </h1>} />
-        <Route path="/tenants" element={<Tenants />} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
-        <Route path="/feedback" element={<h1>Feedback Page</h1>} />
-        <Route path="/login" element={<h1>Login Page</h1>} />
-        <Route path="/register" element={<h1>Register Page</h1>} />
+        <Route
+          path="/"
+          element={
+            <div
+              className="HomePage1"
+              style={{ display: "flex", padding: "20px" }}
+            >
+              <h5 style={{ textAlign: "left", color: "white" }}>
+                Whether you're a property owner, a tenant, or looking for expert
+                advice, we're here to help. Our team is dedicated experience for
+                both property owners and residents.
+              </h5>    <img
+                src="./../public/images/gif1.gif"
+                alt="Home Page GIF"
+                style={{ width: "30%", height: "auto", marginBottom: "20px" }}
+              />
+            </div>
+          }
+        />
+        <Route path="/Tenants" element={<Tenants />} />
+        <Route
+          path="/contact"
+          element={<h1 style={{ color: "white" }}>Contact Page</h1>}
+        />
+        <Route
+          path="/feedback"
+          element={<h1 style={{ color: "white" }}>Feedback Page</h1>}
+        />
+        <Route
+          path="/login"
+          element={<h1 style={{ color: "white" }}>Login Page</h1>}
+        />
+        <Route
+          path="/register"
+          element={<h1 style={{ color: "white" }}>Register Page</h1>}
+        />
       </Routes>
     </Router>
   );
--- client/src/components/Home1.jsx
+++ client/src/components/Home1.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home1.css';
 
-export class Home1 extends Component {
-
-  render() {
-    return (
-      <div>
-        <nav className="NavBar">
-          <ul className="NavList">
-            <img src='./../../public/images/property_logo.jpg' width={"20vh"} alt="logo" />
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/tenants">Tenants</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/feedback">Feedback</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
-}
+const Home1 = () => {
+  return (
+    <div>
+      <nav className="BlackBackground">
+        <ul className="NavList">
+          <img 
+            src="./../../public/images/home.png" 
+            width="20vh" 
+            alt="logo" 
+          />
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/tenants">Tenants</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/feedback">Feedback</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Home1;
