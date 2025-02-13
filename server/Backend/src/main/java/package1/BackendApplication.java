5a89269106be0957000c505daa79f2656f1d95d2
yasaswini <gyasu1110@gmail.com>
2025-02-13 19:35:10 +0530

    coding in java

--- server/Backend/src/main/java/package1/BackendApplication.java
+++ server/Backend/src/main/java/package1/BackendApplication.java
@@ -2,6 +2,7 @@ package package1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//this is main file for backened
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +10,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
 
 }
