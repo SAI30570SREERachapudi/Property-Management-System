2e5864fe80ad18c3ddffd9be68a1466ce6bf5b5d
yasaswini <gyasu1110@gmail.com>
2025-03-09 14:36:12 +0530

    coding in react add propertyMaager

---
+++ client/src/components/PropertyManager.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PropertyManager extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PropertyManager
